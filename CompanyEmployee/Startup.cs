390f6a1ead3cde5bf86c8bcc5144b765baf7d7fd
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-05-02 17:53:36 +0100

    Add project files.

---
+++ CompanyEmployee/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog;
+using CompanyEmployee.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.IO;
+
+namespace CompanyEmployee
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            services.AddControllers();
+            services.ConfigureLoggerService();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            }) ;
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,42 @@
+﻿using Contracts;
+
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+
+    
+{
+    public class LoggerManager : IloggerManager
+
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+            throw new NotImplementedException();
+        }
+
+        public void LogErrr(string message)
+        {
+            logger.Error(message);
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);   
+            throw new NotImplementedException();
+        }
+
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+            throw new NotImplementedException();
+        }
+    }
+}

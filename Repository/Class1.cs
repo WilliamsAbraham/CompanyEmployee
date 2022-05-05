4b5ad760a9b572eb6778828d76429eadd7b7bc1b
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-05-05 13:12:51 +0100

    Generic Repository

---
+++ Repository/Class1.cs
@@ -0,0 +1,45 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase <T>: IRepositoryBase<T> where T: class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        public RepositoryBase (RepositoryContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+
+
+        public IQueryable<T> FindAll(bool TrackChanges)
+        {
+            !TrackChanges ? RepositoryContext.Set<T>()
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(System.Linq.Expressions.Expression<Func<T, bool>> expression, bool TrackChanges)
+        {
+            throw new NotImplementedException();
+        }
+        public void Create(T entity)
+        {
+           
+        }
+        public void Update(T entity)
+        {
+            
+        }
+        public void Delete(T entity)
+        {
+
+        }
+
+    }
+}

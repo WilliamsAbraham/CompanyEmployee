6475e67302a3fa83c94786468534bca628834260
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-06-22 14:36:55 +0100

    Major Refactoring

--- CompanyEmployee/Extensions/ServiceExtensions.cs
+++ CompanyEmployee/Extensions/ServiceExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using Entities;
+using Entities.RepositoryContracts;
 
 namespace CompanyEmployee.Extensions
 {
